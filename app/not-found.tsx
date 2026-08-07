a5a6adf59b7c2cf4e40f16bac406356d7943e12a
simplysmartmoney159-afk <simplysmartmoney159@gmail.com>
2026-08-07 14:21:42 +0100

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
           Page Not Found
         </h2>
         <p className="text-navy-700 mb-8">
-          Sorry, the page you're looking for doesn't exist. It might have been moved or deleted.
+          Sorry, the page you are looking for does not exist. It might have been moved or deleted.
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
           <Link
