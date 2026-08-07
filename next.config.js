2b10db17fdf5803573b348a42de720c8ef146e14
simplysmartmoney159-afk <simplysmartmoney159@gmail.com>
2026-08-07 14:10:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
+  
   images: {
     unoptimized: false,
     domains: ['images.unsplash.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/blog/index',
+        destination: '/blog',
+        permanent: true,
+      },
+    ];
   },
 };
 
